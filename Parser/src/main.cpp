d83aa82cd67f4b6740bcb4194043ee00d804edcf
Eugene <eugene@Eugenes-MacBook-Pro.local>
2017-08-04 00:59:03 +0300

    Validations added

--- Parser/src/main.cpp
+++ Parser/src/main.cpp
@@ -19,39 +19,58 @@ private:
     std::string company;
     std::string data;
     std::atomic<int> starCount;
+    bool isOkay = true;
     
+    //curl writedata func
     static size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up) {
         static_cast<Search*>(up)->data.append(buf, size*nmemb);
-        return size*nmemb;  //tell curl how many bytes we handled
+        return size*nmemb;                          //tell curl how many bytes we handled
     }
     
+    void PrintError(std::string error) {
+        std::cout << "ERROR: " << error << std::endl;
+        isOkay = false;
+    }
+    
+    //curl making request
     void getReposFromGithub() {
         std::string request;
-        request = "http://api.github.com/users/" + company + "/repos";
+        request = "http://api.github.com/users/" + this->company + "/repos";
         std::cout << "request is: " << request << std::endl;
         
         CURL *curl;
         curl_global_init(CURL_GLOBAL_ALL);
         curl = curl_easy_init();
         
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Ogerets");
-        curl_easy_setopt(curl, CURLOPT_URL, request.c_str());
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &Search::writeCallback);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);    //tell curl to output its progress
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "Ogerets");
+            curl_easy_setopt(curl, CURLOPT_URL, request.c_str());
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &Search::writeCallback);
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);    //tell curl to output its progress
         
-        curl_easy_perform(curl);
-        
+            curl_easy_perform(curl);
+        }
+        else
+            PrintError("curl init falied");
+
         //std::cout << std::endl << data << std::endl;
         
         curl_easy_cleanup(curl);
         curl_global_cleanup();
     }
     
+    //json parsing
     void countStars() {
         json j = json::parse(data);
-       
+        
+        auto m = j.find("message");
+        if ((m != j.end()) && (*m == "Not Found")) {
+            PrintError("Company \"" + company + "\" not found");
+            return;
+        }
+        
         int num_cpus = std::thread::hardware_concurrency();
         int part = int(j.size())/num_cpus;
         int rem = j.size() % num_cpus;
@@ -63,7 +82,7 @@ private:
                     for (int i = beg; i < end; i++) {
                         auto it = j[i].find("stargazers_count");
                         if (it != j[i].end()) {
-                            std::cout << *it << std::endl;
+                            //std::cout << *it << std::endl;
                             starCount += int(*it);
                         }
                     }
@@ -72,17 +91,12 @@ private:
             }
             future.wait();
         }
-        /*
-        auto m = j.find("message");
-        if ((m != j.end()) && (*m == "Not Found"))
-            std::cout << "company not found!" << std::endl;
-        */
     
         for (int i = part*num_cpus; i < num_cpus*part + rem; i++) {
             auto it = j[i].find("stargazers_count");
             if (it != j[i].end()) {
-                std::cout << *it << std::endl;
-                starCount += int(*it);
+                //std::cout << *it << std::endl;
+                this->starCount += int(*it);
             }
         }
     }
@@ -90,25 +104,30 @@ private:
 public:
     Search(std::string company) {
         this->company = company;
-        starCount = 0;
+        this->starCount = 0;
     }
     
     void start() {
         getReposFromGithub();
         if (data != "")
             countStars();
-        std::cout << std::endl << starCount << std::endl;
+        else
+            PrintError("data is missing");
+        
+        if (isOkay)
+            std::cout << std::endl << company + "'s total number of stars: " << starCount << std::endl;
     }
 };
 
 
 int main()
 {
-    std::string s;
+    std::string company;
     std::cout << "Type company name:" << std::endl;
-    std::cin >> s;
-    //s = "fastlane";
-    Search s1(s);
+    std::cin >> company;
+    //company = "fastlane";
+    
+    Search s1(company);
     s1.start();
     
     return 0;
